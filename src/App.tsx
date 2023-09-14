36f581e0c2cd35e2b7cab5b9e9bae936f3921e91
Thein Htut <theinhtut.dec@gmail.com>
2023-09-15 00:10:09 +0200

    Added new feat paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <p>Added new line</p>
+        <p>Added new line 123</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
